aa6f843bfbc081b1b0f03f9879ca7020772f7b36
arisfariz26 <arisfariz26@gmail.com>
2022-09-08 23:27:15 +0700

    integration email after register and after checkout

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -8,8 +8,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests\User\Checkout\Store;
 use App\Models\Camp;
 use Auth;
-// use Illuminate\Support\Facades\Auth;
-
+use Mail;
+use App\Mail\Checkout\AfterCheckout;
 
 class CheckoutController extends Controller
 {
@@ -63,6 +63,9 @@ class CheckoutController extends Controller
         //create checkout
         $checkout = Checkout::create($data);
 
+        //sending email
+        Mail::to(Auth::user()->email)->send(new AfterCheckout($checkout));
+
         return redirect(route('checkout.success'));
     }
 
@@ -114,5 +117,10 @@ class CheckoutController extends Controller
     public function success()
     {
         return view('checkout.success');
+    }
+
+    public function invoice(Checkout $checkout)
+    {
+        return $checkout;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::middleware(['auth'])->group(function () {
 
     // User dashboard
     Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+    Route::get('dashboard/checkout/invoice/{checkout}', [CheckoutController::class, 'invoice'])->name('user.checkout.invoice');
 });
 
 
